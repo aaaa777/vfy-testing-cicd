c00067bed4763a36943fc3e1f4ea8d15fa2d0022
logiteca7 <aaaa777@users.noreply.github.com>
2023-02-23 00:26:08 +0000

    [fix] invent tunnel rename on restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # set repo url
-
 if [ -n "$1" ]; then
   REPO_URL=$1
 fi
@@ -10,12 +9,15 @@ if [ -n "$REPO_URL" ]; then
   git clone $REPO_URL
 fi
 
-
 # set container id tunnel name
-
 if [ -z "$TUNNEL_NAME" ]; then
   TUNNEL_NAME="container-$(echo $HOSTNAME | cut -c 1-10)"
 fi
 
-vscode-server rename --accept-server-license-terms --name $TUNNEL_NAME && \
+# set tunnel name
+if [ ! -e "/root/.vscode-cli/code_tunnel.json" ]; then
+  vscode-server rename --accept-server-license-terms --name $TUNNEL_NAME
+fi
+
+# launch
 vscode-server
